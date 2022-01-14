d1ecfddc249dfd5ebcec591575a1539790883fe9
subir <subir.ksoftware@gmail.com>
2022-01-14 12:32:04 +0530

    women page problem resolve

--- components/Women/WomenAllComponents.js
+++ components/Women/WomenAllComponents.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Heading from './Heading';
 import Banner from '../banner/Banner'
 import Service from './Service';
-
+import FooterComponent from '../custom/sections/FooterComponent';
 const WomenAllComponents = () => {
     return (
         <div>
