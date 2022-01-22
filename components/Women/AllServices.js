b3a0e424953acb070f91ce1af9e0c1c250835398
subir <subir.ksoftware@gmail.com>
2022-01-22 22:31:28 +0530

    all service

--- components/Women/AllServices.js
+++ components/Women/AllServices.js
@@ -7,7 +7,6 @@ const AllServices = () => {
   const [category,setCategory]= useState()
   const [first, setfirst] = useState(false);
   let id = localStorage.getItem("value");
-  console.log(id)
   useEffect(() => {
     let city = {
       "CityId": id
@@ -25,29 +24,6 @@ const AllServices = () => {
       });
   },[id]);
   console.log("category",category)
-    const data = [
-        { id: 1, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 2, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 3, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 4, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 5, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 6, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 7, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 8, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 9, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 10, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 11, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 12, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 13, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 14, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 15, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 16, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 17, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 18, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 19, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-        { id: 20, image: 'https://images.unsplash.com/photo-1505944270255-72b8c68c6a70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzV8fHNwYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60', des: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',title:'waxaing' },
-    ]
-    
     return (
         <div style={{backgroundColor:'#F5F5F5',width:'100%',marginTop:'20px',padding:'20px'}}>
 
